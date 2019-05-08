da3ebf73731f0124bf57ddcc0bac51e5d21a9a63
John Williams <ponix4k@gmail.com>
2019-05-08 21:10:56 +0100

    Update shakesenser.ino

--- shakesenser.ino
+++ shakesenser.ino
@@ -1,73 +1,55 @@
-#include <TimerOne.h>
-#define maxArray 64
-int a            = 0;   // in array
-int Tick         = 0;
-int ButtonPin    = 2;  //This is for the button to rest the tick 
-bool isButtonPressed = false;
-int sensorPinX   = A1;  // select the input pin for X Sensor
-int sensorValueX = 0; // variable to store the value coming from the sensor
-int offsetX      = 355;
-
-int sensorPinY = A2;  // select the input pin for Y Sensor
-int sensorPinZ = A3;  // select the input pin for Z Sensor
-
-int sensorValueY = 0; // variable to store the value coming from the sensor
-int sensorValueZ = 0; // variable to store the value coming from the sensor
-int arrayHeadX = 0;   // start of the array
-int arrayTailX = 0;   // end of the array
-int arrayX[maxArray];       // array 
-int ledPin = 13;      // select the pin for the LED
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis 
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis
+int Xoffset;
+int Yoffset;
+int Zoffset;
+int RED = 12;
+int GREEN = 11;
+int YELLOW = 10;
+int t1 = 500;
 
 void setup() {
   Serial.begin(115200);
-  Timer1.initialize(1000); // this value is in micro seconds (1000 Micro Seconds = 1 MilliSecond)
-  Timer1.attachInterrupt(ReadSensors);
-  pinMode(ButtonPin, INPUT);
- }
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
+  Xoffset = analogRead(xpin);
+  Yoffset = analogRead(ypin);
+  Zoffset = analogRead(zpin);
+}
 
-void ReadSensors(){
+void loop() {
   
-    // read the value from the sensors:
-  Tick++; //counter for time
-  arrayX[arrayHeadX] = analogRead(sensorPinX)-offsetX;
-  arrayHeadX++;
-  if (arrayHeadX > maxArray -1){ 
-    arrayHeadX = 0;
-    }
-
-  if (arrayHeadX==arrayTailX){
-    if(arrayHeadX > 0){
-      arrayHeadX--;
-      }
-    else{
-      arrayHeadX = 63;
-    }
-}
-  //sensorValueY = analogRead(sensorPinY);
-  //sensorValueZ = analogRead(sensorPinZ)-339;
-}
-
-void loop() {     //main loop
-  //Serial.print("loops brother");
-isButtonPressed =digitalRead(ButtonPin);
-//Serial.println(digitalRead(ButtonPin));
-//Serial.print(isButtonPressed + "Button pressed");
-if (isButtonPressed){
-  Serial.println("sensor reset");
-  Tick = 0;
-  delay(5000);
-} 
-
-while (Tick <= 6000000){ //tick is in micro seconds as it's in use in the interrupt 
-  delay(100); // this vaule is in milliseconds
   Serial.print("X: ");
-  for (a = 0;a < arrayHeadX;a++){
-    Serial.print(arrayX[a]);
-    Serial.println(", "); 
- }
- 
-}
-
- Serial.println("###");
- arrayHeadX= 0;  
+  Serial.print(analogRead(xpin)-(5));
+  Serial.print("\t");
+  
+  //digitalWrite(RED,HIGH);
+  //delay(t1);
+  //digitalWrite(RED,LOW);
+  //delay(t1);
+  
+  Serial.print("Y: ");
+  Serial.print(analogRead(ypin)-(5));
+  Serial.print("\t");
+  
+  //digitalWrite(GREEN,HIGH);
+  //delay(t1);
+  //digitalWrite(GREEN,LOW);
+  //delay(t1);
+  
+  Serial.print("Z: ");
+  Serial.print(analogRead(zpin)-(5));
+  Serial.println();
+  
+  //digitalWrite(YELLOW,HIGH);
+  //delay(t1);
+  //digitalWrite(YELLOW,LOW);
+  //delay(t1);
+  // delay before next reading:
+  delay(10);
 }
