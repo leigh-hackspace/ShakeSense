bc5a86c97de86c75cd83ea6a67420363c4d256fe
John Williams <ponix4k@gmail.com>
2019-05-09 21:20:51 +0100

    added LEDs
    rewrite the main file

--- shakesenser.ino
+++ shakesenser.ino
@@ -3,13 +3,69 @@ const int powerpin = 19;              // analog input pin 5 -- voltage
 const int xpin = A3;                  // x-axis 
 const int ypin = A2;                  // y-axis
 const int zpin = A1;                  // z-axis
+
 int Xoffset;
 int Yoffset;
 int Zoffset;
-int RED = 12;
+
+int YELLOW = 12;
 int GREEN = 11;
-int YELLOW = 10;
-int t1 = 500;
+int RED = 10;
+
+/*
+ * These are the activation tresh holds for the LEDs which is calculated from 
+ * from the current value minus the previous vaule to get the difference 
+ * e.g. A - B = C the threshold value is C
+*/
+const int RED_THRESHOLD = 100; 
+const int YELLOW_THRESHOLD = 50;
+const int GREEN_THRESHOLD = 25;
+
+struct SensorValues
+{
+  int xValue;
+  int yValue;
+  int zValue;
+
+  SensorValues()
+    : xValue(0)
+    , yValue(0)
+    , zValue(0)
+  {}
+
+  int LengthSqr()
+  {
+    //a^2 = b^2 + c^2 + d^2
+    return (xValue  * xValue) +  (yValue  * yValue) +  (zValue  * zValue);
+  }
+
+  SensorValues GetDifference( const SensorValues other ) const
+  {
+    SensorValues result;
+    result.xValue = abs(xValue - other.xValue);
+    result.yValue = abs(yValue - other.yValue);
+    result.zValue = abs(zValue - other.zValue);
+
+    return result;
+  }  
+
+  void Report()
+  {
+    Serial.print("X: ");
+    Serial.print( xValue );
+    Serial.print("\t");  
+
+    Serial.print("Y: ");
+    Serial.print( yValue );
+    Serial.print("\t");  
+
+    Serial.print("Z: ");
+    Serial.print( zValue );
+    Serial.print("\t\n");  
+  }
+};
+
+SensorValues previousReading;
 
 void setup() {
   Serial.begin(115200);
@@ -22,34 +78,40 @@ void setup() {
   Zoffset = analogRead(zpin);
 }
 
+void set_led( int light, int intensity, int activationThreshold )
+{
+  if( intensity > ( activationThreshold * activationThreshold ) )
+  {
+    digitalWrite(light,HIGH);
+  }
+  else
+  {
+    digitalWrite(light,LOW);
+  }
+}
+
+void flash_led(SensorValues & newReading ){
+
+  SensorValues difference = newReading.GetDifference(previousReading);
+  previousReading = newReading; 
+  difference.Report();
+  
+  int intensity = difference.LengthSqr();
+
+  set_led(RED,    intensity, RED_THRESHOLD);
+  set_led(YELLOW, intensity, YELLOW_THRESHOLD);
+  set_led(GREEN,  intensity, GREEN_THRESHOLD);
+}
+
 void loop() {
-  
-  Serial.print("X: ");
-  Serial.print(analogRead(xpin)-(5));
-  Serial.print("\t");
-  
-  //digitalWrite(RED,HIGH);
-  //delay(t1);
-  //digitalWrite(RED,LOW);
-  //delay(t1);
-  
-  Serial.print("Y: ");
-  Serial.print(analogRead(ypin)-(5));
-  Serial.print("\t");
-  
-  //digitalWrite(GREEN,HIGH);
-  //delay(t1);
-  //digitalWrite(GREEN,LOW);
-  //delay(t1);
-  
-  Serial.print("Z: ");
-  Serial.print(analogRead(zpin)-(5));
-  Serial.println();
-  
-  //digitalWrite(YELLOW,HIGH);
-  //delay(t1);
-  //digitalWrite(YELLOW,LOW);
-  //delay(t1);
+
+  SensorValues reading;
+  reading.xValue = analogRead(xpin);
+  reading.yValue = analogRead(ypin);
+  reading.zValue = analogRead(zpin);
+
+  flash_led(reading);
+
   // delay before next reading:
   delay(10);
 }
